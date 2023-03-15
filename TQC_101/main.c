70e6eee61a9b0cf5e0de27f8c2791b46fce325e3
evachen1010 <a0937510149@gmail.com>
2023-03-15 09:29:58 +0800

    20230315_2

--- TQC_101/main.c
+++ TQC_101/main.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <stdio.h> //引入stdio.h(標準輸入輸出標頭檔案)
 
 int main()
 {
     int input;
-    scanf("%d", &input);
-    input  += 9;
-    printf("%d", input);
+    scanf("%d", &input); // 輸入
+    input += 9;          // 相加後賦值
+    printf("%d", input); // 輸出
 
     return 0;
 }
